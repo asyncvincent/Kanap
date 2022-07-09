fbbfaff89a38a7fcdf8daeb10e16f5756a92902c
Vincent <106317355+asyncvincent@users.noreply.github.com>
2022-07-09 14:00:09 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,75 +1,67 @@
-//get product details from local storage
-let productId = localStorage.getItem('productId');
-let productName = localStorage.getItem('productName');
-let productPrice = localStorage.getItem('productPrice');
-let productImage = localStorage.getItem('productImage');
-let productAlt = localStorage.getItem('productAlt');
-let productDescription = localStorage.getItem('productDescription');
-let productColors = localStorage.getItem('productColors');
-let productQuantity = localStorage.getItem('productQuantity');
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
+const cart = localStorage.getItem('cart');
+const cartDetails = JSON.parse(cart);
 
-console.log(cart);
-//add product to cart
-function addProductToCart() {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let product = {
-        id: productId,
-        name: productName,
-        price: productPrice,
-        image: productImage,
-        alt: productAlt,
-        description: productDescription,
-        colors: productColors,
-        quantity: productQuantity
-    };
-    cart.push(product);
-    localStorage.setItem('cart', JSON.stringify(cart));
+if (cart === null) {
+    window.location.href = './index.html';
 }
 
 
-//display cart
-function displayCart() {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let cartList = document.getElementById('cartList');
-    let cartTotal = document.getElementById('cartTotal');
-    let cartTotalPrice = 0;
-    let cartTotalQuantity = 0;
-    cartList.innerHTML = '';
-    cart.forEach(product => {
-        let cartItem = document.getElementById('cart__items');
-        //cartItem.classList.add('cart__item');
-        cartItem.innerHTML = `
-        <article class="cart__item" data-id="{id}" data-color="{colors}">
-            <div class="cart__item__img">
-            <img src="${image}" alt="${alt}">
-            </div>
-            <div class="cart__item__content">
-            <div class="cart__item__content__description">
-                <h2>${name}</h2>
-                <p>Vert</p>
-                <p>42,00 €</p>
-            </div>
-            <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+// display cart details
+async function getCartDetails() {
+    let cartQuantity = 0;
+    cartDetails.forEach(product => {
+        cartQuantity += product.quantity;
+    });
+    document.getElementById('totalQuantity').innerHTML = cartQuantity;
+
+    let cartPrice = 0;
+    cartDetails.forEach(product => {
+        cartPrice += product.price * product.quantity;
+    });
+    document.getElementById('totalPrice').innerHTML = cartPrice;
+    const cartDetailsList = document.getElementById('cart__items');
+    cartDetailsList.innerHTML = cartDetails.map(product => {
+        return `
+            <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+                <div class="cart__item__img">
+                    <img src="${product.image}" alt="${product.altImg}">
                 </div>
-                <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
+                <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>${product.name}</h2>
+                        <p>${product.color}</p>
+                        <p>${product.price} €</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                            <p>Qté : ${product.quantity}</p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="0">
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-        </article>
-        `;
-        cardList = document.querySelector('.cart');
-        cartList.appendChild(cartItem);
-        cartTotalPrice += parseInt(product.price) * parseInt(product.quantity);
-        cartTotalQuantity += parseInt(product.quantity);
-    }
-    );
-    cartTotal.innerHTML = `
-        <p class="cart__total__price">Total: ${cartTotalPrice}</p>
-        <p class="cart__total__quantity">Quantity: ${cartTotalQuantity}</p>
-    `;
+            </article>`;
+    }).join('');
 }
+
+getCartDetails();
+
+// use itemQuantity to update cart details and localStorage when user changes quantity of a product in cart
+const itemQuantity = document.querySelectorAll('.itemQuantity');
+itemQuantity.forEach(input => {
+    input.addEventListener('change', () => {
+        const productId = input.parentElement.parentElement.parentElement.parentElement.dataset.id;
+        const productColor = input.parentElement.parentElement.parentElement.parentElement.dataset.color;
+        const productQuantity = input.value;
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        cart.forEach(product => {
+            if (product.id === productId && product.color === productColor) {
+                product.quantity = productQuantity;
+            }
+        });
+        localStorage.setItem('cart', JSON.stringify(cart));
+        getCartDetails();
+    });
+});
